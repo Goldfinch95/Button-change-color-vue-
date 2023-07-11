ce4bf41b9858f508039c982e5af530934d1a183f
Goldfinch <facu_vila@hotmail.com>
2023-07-10 23:39:19 -0300

    button change color

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script>
+import Button from "./components/Button.vue";
+export default {
+  data() {
+    return {
+      buttons: [
+        {
+          id: "1",
+          buttonColor: "black",
+          count: 0,
+        },
+        {
+          id: "2",
+          buttonColor: "black",
+          count: 0,
+        },
+        {
+          id: "3",
+          buttonColor: "black",
+          count: 0,
+        },
+        {
+          id: "4",
+          buttonColor: "black",
+          count: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    ChangeColor(button) {
+      button.count++;
+      if (button.count >= 10) button.buttonColor = "+10";
+    },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<template>
+  <div class="box_buttons">
+    <div class="button_container">
+      <Button
+        v-for="button in buttons"
+        @click="ChangeColor(button)"
+        :key="button.id"
+        :button="button"
+        :class="button.buttonColor === '+10' ? 'boquita' : ''"
+      />
+    </div>
+  </div>
+</template>
+
+<style>
+.box_buttons {
+  display: flex;
+  height: auto;
+  width: 100%;
+  padding: 15px;
+  margin-bottom: 150px;
+  border: none;
+  /*background-color: #003391;*/
+  border-radius: 12px;
+  color: #fff;
+}
+.boquita {
+  background-color: #f0b029;
+  color: #02236d;
+}
+.button_container {
+  width: 60vw;
+  height: 50vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: {
+    button: {
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+};
+</script>
+<template>
+  <button>Clickeaste {{ button.count }} veces</button>
+</template>
+<style>
+button {
+  height: 10%;
+  width: 40vw;
+  margin: 15px;
+  margin-top: 30px;
+  background-color: #02236d;
+  border-radius: 7px;
+  font-size: 15px;
+  border: none;
+  color: #f0b029;
+}
+button:focus {
+  border: 1px solid #f0b029;
+}
+</style>
